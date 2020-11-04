709bca831f737246d33daff4154e719250f1114c
robertpm <grobertpm@gmail.com>
2020-11-04 01:37:52 -0800

    migration tests

--- seed/migrations/20201016081712-create-truth.js
+++ seed/migrations/20201016081712-create-truth.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       truth: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         unique: true
       },
       argument: {
