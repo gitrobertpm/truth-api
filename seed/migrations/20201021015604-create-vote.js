563a9e7c7d766f91abc98c07b985ee9fc789ae36
robertpm <grobertpm@gmail.com>
2021-04-29 00:39:37 -0700

    Tried to set custom UUID in seeders

--- seed/migrations/20201021015604-create-vote.js
+++ seed/migrations/20201021015604-create-vote.js
@@ -4,7 +4,6 @@ module.exports = {
     await queryInterface.createTable("Votes", {
       id: {
         type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
         primaryKey: true,
         allowNull: false
       },
